a8cfe29af4a9d74f96343dd56d6dd3afd4475a20
-_- Zaba -_- <bsilva@korewa.com.br>
2011-08-05 11:34:46 -0300

    Inicio do Projeto Novo Site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KwFasterm::Application.config.secret_token = 'ddd9b2c93742ca62d01e05877e1753738b3cce9ba3c73637a93f13bed8e5898af38eb1a8bb399b0ebed101f3e6027906ff5fc473ab5f2ebb117e4a0143a94d5e'
