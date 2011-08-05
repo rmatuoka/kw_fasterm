f8fc987feadeb458a620a2f496ad7aa380bddaf5
-_- Zaba -_- <bsilva@korewa.com.br>
2011-08-05 16:57:12 -0300

    Ie 6 não

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def navegadorie6
-    if browser.safari?
+    if browser.ie6?
       redirect_to atualizar_index_path
     end 
-    if browser.firefox?
-      redirect_to atualizar_index_path
-    end
-    if browser.chrome?
-      redirect_to atualizar_index_path
-    end
   end
+  
+  
 end
