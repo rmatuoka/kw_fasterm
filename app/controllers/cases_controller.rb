ad94bfc053364f5df4cae7ab2dc31c611c34f4f1
-_- Zaba -_- <bsilva@korewa.com.br>
2011-09-12 15:05:58 -0300

    Ajuste Cases

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,12 +1,16 @@
 class CasesController < ApplicationController
   before_filter :navegadorie6
   def index
+    @Cases = Case.all(:conditions => ['published = 1'], :order => "`cases`.`category`")
   end
 
   def categoria
   end
 
   def show
+    @Case = Case.find(params[:id])
+    #SELECIONA IMAGENS
+    @Imagens = @Case.case_images.all
   end
 
 end
