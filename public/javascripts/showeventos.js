6e73cd0ee27b807ee2894b1789f5a11242236ab8
-_- Zaba -_- <bsilva@korewa.com.br>
2011-09-12 17:54:05 -0300

    Eventos

--- public/javascripts/showeventos.js
+++ public/javascripts/showeventos.js
@@ -1,66 +1,31 @@
-function show_session(x, obj)
-{
-	
-	//MOTRA AS LOJAS
-	if(jQuery("#box_cases" + x).hasClass('hide'))
-	{
-		//MOSTRA
-		jQuery("#box_cases" + x).slideDown('slow');
-		jQuery("#box_cases" + x).removeClass('hide')
-	}
-	else
-	{
-		//ESCONDE\
-		jQuery("#box_cases" + x).slideUp('slow');
-		jQuery("#box_cases" + x).addClass('hide')
-	}
-	//MUDA O FUNDO DO BACKGROUND
-	if(jQuery(obj).hasClass("tit_case_mais"))
-	{	
-		//CLICADO
-		//alert("fechar");
-		jQuery(obj).removeClass('tit_case_mais');
-		jQuery(obj).addClass('tit_case_menos')
-	}
-	else
-	{
-		//alert("abrir");
-		jQuery(obj).removeClass('tit_case_menos');
-		jQuery(obj).addClass('tit_case_mais')		
-	}
-	
-	//alert(jQuery(obj).attr("class"));
-}
-
 function show_eventos(x, obj)
 {
-	
 	//MOTRA AS LOJAS
-	if(jQuery("#box_cases" + x).hasClass('hide'))
+	if(jQuery("#eventos_body" + x).hasClass('hide'))
 	{
 		//MOSTRA
-		jQuery("#box_cases" + x).slideDown('slow');
-		jQuery("#box_cases" + x).removeClass('hide')
+		jQuery("#eventos_body" + x).slideDown('slow');
+		jQuery("#eventos_body" + x).removeClass('hide')
 	}
 	else
 	{
 		//ESCONDE\
-		jQuery("#box_cases" + x).slideUp('slow');
-		jQuery("#box_cases" + x).addClass('hide')
+		jQuery("#eventos_body" + x).slideUp('slow');
+		jQuery("#eventos_body" + x).addClass('hide')
 	}
 	//MUDA O FUNDO DO BACKGROUND
-	if(jQuery(obj).hasClass("tit_case_mais"))
+	if(jQuery(obj).hasClass("evt_mais"))
 	{	
 		//CLICADO
 		//alert("fechar");
-		jQuery(obj).removeClass('tit_case_mais');
-		jQuery(obj).addClass('tit_case_menos')
+		jQuery(obj).removeClass('evt_mais');
+		jQuery(obj).addClass('evt_menos')
 	}
 	else
 	{
 		//alert("abrir");
-		jQuery(obj).removeClass('tit_case_menos');
-		jQuery(obj).addClass('tit_case_mais')		
+		jQuery(obj).removeClass('evt_menos');
+		jQuery(obj).addClass('evt_mais')		
 	}
 	
 	//alert(jQuery(obj).attr("class"));
