902b9b374467ddfc7ab1ec92411b5a5eed8d679d
Rafael Matuoka <rmatuoka@korewa.com.br>
2012-05-16 18:27:51 -0300

    Ajustes planilha

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -9,12 +9,14 @@ class SimulationsController < ApplicationController
     @LITROS_MOTEL = 80
     @LITROS_HOSPITAL = 80
     @LITROS_ACADEMIA = 80
+    @LITROS_CONDOMINIO = 80
     
     @GRAU_DE_AQUECIMENTO = 35
     @KCAL_FT = 22000.00
     @KCAL_FT_UN = 7700
     @KCAL_GLP_UN = 2.8
     @KCAL_DIESEL_UN = 3
+    @KCAL_GN_UN = 3
     @KCAL_ENERGIA_UN = 20000
     
     
@@ -22,10 +24,12 @@ class SimulationsController < ApplicationController
     @CUSTO_GLP = 0.00
     @CUSTO_DIESEL = 0.00
     @CUSTO_ENERGIA = 0.00
+    @CUSTO_GN = 0.00
     
     #CO2
     @CO2_BASE_DIESEL = 3.11
     @CO2_BASE_GLP = 2.91
+    @CO2_BASE_GN = 2.91
     
     @TOTAL_KG_CO2 = 0.00
     
@@ -35,7 +39,15 @@ class SimulationsController < ApplicationController
     #FONTES DE ENERGIA
     @E_GLP = 2.50
     @E_DIESEL = 2.10
-    @E_ENERGIA = 0.42
+    
+    if !params[:valor_energia_e].blank?
+      @E_ENERGIA = params[:valor_energia_e].to_f
+    else
+      @E_ENERGIA = 0.42
+    end
+    
+    puts "AAA" + @E_ENERGIA.to_s
+    @E_GN = 2.3
     @E_USUARIO = 0
     
     @Economia = 0.00
@@ -75,6 +87,14 @@ class SimulationsController < ApplicationController
       total_litros = ((params[:n_banhos_academia]).to_i * @LITROS_ACADEMIA).to_i
       
       puts "==> TOTAL DE LITROS " + total_litros.to_s
+      
+    when "5"
+      puts "=> CONDOMINIO"
+      
+      total_litros = ((params[:n_apartamentos_cond]).to_i * (params[:n_leitos_cond]).to_i * @LITROS_HOTEL * (100).to_f).to_i
+      
+      puts "==> TOTAL DE LITROS " + total_litros.to_s
+    
     end
     
     #CALCULA O CUSTO FT
@@ -121,8 +141,26 @@ class SimulationsController < ApplicationController
       @CUSTO_ENERGIA = calcula_custo(calcula_horas_necessarias(total_litros), "energia")
       puts "CUSTO ENERGIA " + @CUSTO_ENERGIA.to_s
       @Economia = 100 - ((@CUSTO_FT / @CUSTO_ENERGIA) * 100).round(2)
+      
+    when "4"
+      puts "GN"
+      @CUSTO_GN = calcula_custo(calcula_horas_necessarias(total_litros), "gn")
+      puts "CUSTO GN " + @CUSTO_GN.to_s
+      #CALCULA CO2
+      if params[:valor_energia].blank?
+        @TOTAL_KG_CO2 = (@CUSTO_GLP / @E_GN).to_f
+      else
+        @TOTAL_KG_CO2 = (@CUSTO_GLP / params[:valor_energia].to_f).to_f
+      end
+      #CO2
+      @TOTAL_CO2_ANUAL = (@TOTAL_KG_CO2 * @CO2_BASE_GN * 12)/1000
+      @TOTAL_CO2_MES = (@TOTAL_KG_CO2 * @CO2_BASE_GN)/1000
+      
+      #RESULTADO
+      @Economia = 100 - ((@CUSTO_FT / @CUSTO_GN) * 100).round(2)
     end
     
+    @Economia = 0 if @Economia < 0
     #RETORNA A % DE ECONOMIA
     puts "% DE ECONOMIA => " + @Economia.to_s
     
@@ -131,7 +169,9 @@ class SimulationsController < ApplicationController
   end
   
   def calcula_custo_ft(horas_de_funcionamento)
+    puts "BBB" + @E_ENERGIA.to_s
     return ((((horas_de_funcionamento * @KCAL_FT_UN) / 100) * @E_ENERGIA) / 10) * 30
+    
   end
   
   def calcula_custo(horas_de_funcionamento, tipo_de_energia)
@@ -150,6 +190,12 @@ class SimulationsController < ApplicationController
           custo = ((horas_de_funcionamento * @KCAL_DIESEL_UN).to_f * @E_DIESEL) * 30
         else
           custo = ((horas_de_funcionamento * @KCAL_DIESEL_UN).to_f * params[:valor_energia].to_f) * 30
+        end
+      when "gn"
+        if params[:valor_energia].blank?
+          custo = ((horas_de_funcionamento * @KCAL_GN_UN).to_f * @E_GN) * 30
+        else
+          custo = ((horas_de_funcionamento * @KCAL_GN_UN).to_f * params[:valor_energia].to_f) * 30
         end
       when "energia"
         if params[:valor_energia].blank?
